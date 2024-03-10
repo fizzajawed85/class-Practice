a342bd825e2e59ddd27b3d807c9c79d7e7c7a2eb
fizzajawed85 <162190155+fizzajawed85@users.noreply.github.com>
2024-03-10 18:27:16 +0500

    Add files via upload

---
+++ Practice.02/main.js
@@ -0,0 +1,9 @@
+"use strict";
+let userName = "Fizza!"; //string
+console.log(userName);
+let age = 22; //number
+console.log(age);
+let question = true; //boolean
+console.log(question);
+let a;
+console.log(a);
---
+++ Practice.02/main.ts
@@ -0,0 +1,16 @@
+
+
+let userName :string = "Fizza!";  //string
+console.log(userName);
+
+let age :number = 22;    //number
+console.log(age);
+
+let question: boolean = true;  //boolean
+console.log(question);
+
+let a: null;
+console.log(a);
+
+
+
