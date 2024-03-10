b107f97f514de873ffcd2307da0cab4fb876250f
fizzajawed85 <162190155+fizzajawed85@users.noreply.github.com>
2024-03-10 18:32:41 +0500

    Add files via upload

---
+++ Practice.00/index.ts
@@ -0,0 +1,9 @@
+let a : number =7;
+let b : number =3;
+console.log("a + b", a + b);
+console.log("a + b", a + b);
+console.log("a + b", a + b);
+console.log("a + b", a + b);
+console.log
+console.log
+console.log
