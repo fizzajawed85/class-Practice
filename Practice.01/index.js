dcb4a484cb5f31836194d10d5ff369fd79ab7045
fizzajawed85 <162190155+fizzajawed85@users.noreply.github.com>
2024-03-10 18:25:29 +0500

    Add files via upload

---
+++ Practice.01/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let message = "Fizza !";
+console.log(message);
+message = "123";
+console.log(message);
---
+++ Practice.01/index.ts
@@ -0,0 +1,4 @@
+let message : string = "Fizza !";
+console.log(message);
+message = "123";
+console.log(message);
